d88e50dfcf7d0d30b74bb21e10d18bc413010cf6
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-01 15:11:25 -0700

    Rewrite SnippetDetail class using APIView

--- snippets/views.py
+++ snippets/views.py
@@ -23,9 +23,30 @@ class SnippetList(APIView):
         return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class SnippetDetail(APIView):
     """
     Retrieve, update or delete a snippet instance.
     """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data.request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
