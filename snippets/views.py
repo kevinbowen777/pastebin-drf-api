2a2b48daa31257bd7e90de1aaf384ce7c52633b6
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-01 15:28:11 -0700

    Remove generics import

--- snippets/views.py
+++ snippets/views.py
@@ -3,7 +3,7 @@ from .serializers import SnippetSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics, status
+from rest_framework import status
 
 
 class SnippetList(APIView):
