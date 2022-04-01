cf9c77351ae4c0ddaad69e33b220cebfdead596c
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-01 14:54:52 -0700

    Prep for rewrite of Classes using APIView

--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,22 @@
-from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, status
 
 
 class SnippetList(generics.ListCreateAPIView):
+    """
+    List all snippets, or create a new snippet.
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
