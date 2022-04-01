556cadefea48652c5d3759ca895b2acb35232794
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-01 15:03:08 -0700

    Update SnippetList with APIView

--- snippets/views.py
+++ snippets/views.py
@@ -6,12 +6,21 @@ from rest_framework.response import Response
 from rest_framework import generics, status
 
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetList(APIView):
     """
     List all snippets, or create a new snippet.
     """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data.request)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
