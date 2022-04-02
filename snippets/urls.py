d1d479c79c1c1e1269e78c4a0be4d91cc54c1247
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-01 17:41:42 -0700

    Add URLs for User views

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # URLs used for class based views
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
     # URLs used for function based views
     # path('snippets/', views.snippet_list),
     # path('snippets/<int:pk>/', views.snippet_detail),
