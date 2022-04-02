188ffc42db09310ba015533565869e7a081e3b8b
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-01 19:58:07 -0700

    Add links between users, snippets, highlights

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,18 +3,21 @@ from rest_framework import serializers
 from .models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id','title', 'code', 'linenos',
+        fields = ('url', 'id', 'highlight', 'title', 'code', 'linenos',
                   'language', 'owner', 'style')
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
