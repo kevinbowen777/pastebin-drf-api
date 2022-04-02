d37236e85d01ad73ebcfd022d8c9b85e0a1c7333
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-01 19:33:24 -0700

    Create named URLs for views. Add api_root URL

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,14 +4,11 @@ from snippets import views
 
 
 urlpatterns = [
-    # URLs used for class based views
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-    # URLs used for function based views
-    # path('snippets/', views.snippet_list),
-    # path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('', views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
