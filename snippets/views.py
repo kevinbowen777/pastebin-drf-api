9878153d45748d8c0373f2755afd530e767f1faa
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-01 17:36:12 -0700

    Add read-only UserList & UserDetail views

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,8 @@
-from .models import Snippet
-from .serializers import SnippetSerializer
+from django.contrib.auth.models import User
 from rest_framework import generics
+
+from .models import Snippet
+from .serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -16,3 +18,11 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
