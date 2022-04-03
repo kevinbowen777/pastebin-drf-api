002c693551de19a3da29a63c7f619ce04ea7abe9
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-02 22:37:03 -0700

    Restore api_root

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework import renderers
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
-from .views import SnippetViewSet, UserViewSet, api_root
+from .views import SnippetViewSet, UserViewSet
 
 
 snippet_list = SnippetViewSet.as_view({
@@ -28,19 +28,10 @@ user_detail = UserViewSet.as_view({
 
 urlpatterns = format_suffix_patterns([
     path('', views.api_root),
-    path('snippets/',
-         views.SnippetList.as_view(),
-         name='snippet-list'),
-    path('snippets/<int:pk>/',
-         views.SnippetDetail.as_view(),
-         name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/',
-         views.SnippetHighlight.as_view(),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight,
          name='snippet-highlight'),
-    path('users/',
-         views.UserList.as_view(),
-         name='user-list'),
-    path('users/<int:pk>/',
-         views.UserDetail.as_view(),
-         name='user-detail'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
 ])
--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import permissions, renderers, viewsets
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
