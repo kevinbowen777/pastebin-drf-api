fea142dae03ead733d2abba0835e12b7396751ce
Kevin Bowen <kevin.bowen@gmail.com>
2022-09-12 12:58:20 -0700

    Correct typos in STATICFILES_FINDERS & STATICFILES_STORAGE settings

--- config/settings.py
+++ config/settings.py
@@ -118,13 +118,11 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (str(BASE_DIR.joinpath("static")),)
 STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
-STATICFILE_FINDERS = [
+STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectorFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-STATICFILE_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage",
-)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
